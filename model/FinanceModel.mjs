b88705e25e3d25892e18390194229e7337c7118d
Dmytro-Kanunik <Dmytro.Kanunik@coxautoinc.com>
2021-08-19 21:04:24 +0300

    add linter

--- model/FinanceModel.mjs
+++ model/FinanceModel.mjs
@@ -1,11 +1,11 @@
 import AppConfig from '../configs/AppConfig.cjs';
 
-import axios from "axios";
+import axios from 'axios';
 
 class FinanceModel {
 
     static async getFinanceData() {
-        const params = 'chart/AAPL?region=US&lang=en-US&includePrePost=false&interval=2m&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance'
+        const params = 'chart/AAPL?region=US&lang=en-US&includePrePost=false&interval=2m&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance';
         try {
             return await axios.get(AppConfig.baseUrl + params);
         } catch (e) {
